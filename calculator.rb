6fa0f381bb05cfdea8b9ec46fd3320abed1587ea
Stephen <stephen@shopkeep.com>
2016-03-27 21:41:19 -0400

    a terribly sloppy calculator

---
+++ calculator.rb
@@ -0,0 +1,112 @@
+require 'pp'
+require 'bigdecimal'
+
+class String
+  def nan?
+    self !~ /^\s*[+-]?((\d+_?)*\d+(\.(\d+_?)*\d+)?|\.(\d+_?)*\d+)(\s*|([eE][+-]?(\d+_?)*\d+)\s*)$/
+  end
+end
+
+module Calculator
+  class Parser
+    attr_reader :string, :array
+    def initialize(calculator_string)
+      @string = calculator_string
+      string_array = string.split(' ')
+      @array = string_array.map { |element| element.nan? ? element : ::BigDecimal.new(element) }
+    end
+
+    def run
+      puts "#{string} = #{generate_expressions(array).evaluate.to_s('F')}"
+    end
+
+    def generate_expressions(parsed_array)
+      if parsed_array.index('(') 
+        generate_expressions(inner_expression(parsed_array))
+      else
+        Expression.new(parsed_array)
+      end
+    end
+
+
+    def is_number?(string)
+      string.to_i.to_s == string
+    end
+
+    def inner_expression(array)
+      left_paren = array.rindex('(')
+      right_paren = array[left_paren..(array.length - 1)].index(')') + left_paren
+      inner_array = array[(left_paren + 1)..(right_paren - 1)]
+      expression = Expression.new(inner_array)
+      ArraySquisher.squish(array, expression, left_paren + 1, inner_array.length)
+    end
+  end
+
+  class Expression
+    attr_reader :expression_array
+    EXPONENT = '^'.freeze
+    MULTIPLICATION = '*'.freeze
+    DIVISION = '/'.freeze
+    ADDITION = '+'.freeze
+    SUBTRACTION = '-'.freeze
+    ORDER_OF_OPERATIONS = [[EXPONENT], [MULTIPLICATION, DIVISION], [ADDITION, SUBTRACTION]].freeze
+
+    def initialize(expression_array = [])
+      @expression_array = expression_array
+    end
+
+    def evaluate
+      inner_evaluation = expression_array.map{ |element| element.class == Expression ? element.evaluate : element }
+      perform_order_of_operations(inner_evaluation).first
+    end
+
+    def perform_order_of_operations(running_evaluation)
+      ORDER_OF_OPERATIONS.each do |operation_set|
+        if (running_evaluation & operation_set).length > 0
+          operation = running_evaluation.find { |element| operation_set.include? element }
+          running_evaluation = evaluate_operation(running_evaluation, operation)
+        end
+      end
+      running_evaluation
+    end
+
+    def evaluate_operation(array, operation_string)
+      if index = array.index(operation_string)
+        new_value = perform_operation(array[index - 1], array[index + 1], operation_string)
+        array = ArraySquisher.squish(array, new_value, index)
+      end
+      perform_order_of_operations(array)
+    end
+
+
+    def perform_operation(value_1, value_2, operation_string)
+      case operation_string
+      when EXPONENT
+        value_1 ** value_2
+      when MULTIPLICATION
+        value_1 * value_2
+      when DIVISION
+        value_1 / value_2
+      when ADDITION
+        value_1 + value_2
+      when SUBTRACTION
+        value_1 - value_2
+      end
+    end
+  end
+end
+
+class ArraySquisher
+  def self.squish(array, new_value, index, length = 1)
+    [].tap do |a|
+      a.push *array[0..(index - 2)] if (index > 2)
+      a.push *new_value
+      a.push *array[(index + length + 1)..(array.length - 1)] if (array.length > index + length + 1)
+    end
+  end
+end
+
+
+puts Calculator::Parser.new('3 * 2 ^ ( ( 3 + 1 ) - 1 *  5 + 2 )').run
+puts Calculator::Parser.new('3 * 2 ^ ( ( 3 + 1 ) - 1 * ( 5 + 2 ) )').run
+puts Calculator::Parser.new('3 * 2 ^ ( ( 3 + 1 ) - 1 * ( 5 + 2 ) ) + 1.1').run
